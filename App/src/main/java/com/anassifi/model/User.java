3dbdea99ed9034f3bc38273a262871310c3dd54f
Anassifi <abdelhakimnassifi@gmail.com>
2021-03-10 21:38:10 +0100

    Project models
    These are my project models, using hibernate annotation.

---
+++ App/src/main/java/com/anassifi/model/User.java
@@ -0,0 +1,68 @@
+package com.anassifi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USER")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_user;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String phone;
+
+    public User() {
+    }
+
+    public long getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(long id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
